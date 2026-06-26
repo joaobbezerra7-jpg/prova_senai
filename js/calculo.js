75991b40fec6caa68818b3e0ef21af646179db17
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-06-26 15:42:12 -0300

    finalizando as estilizações

--- js/calculo.js
+++ js/calculo.js
@@ -7,47 +7,47 @@ function calcularLote(arrayFormas) {
         if (info.valor <= 20000) {
             
             return `Descrição <br> Quadra: ${info.quadra} <br>
-            Lote: ${info.lot} <br> Comprimento: ${info.comp} <br> Largura: ${info.larg} <br>
+            Lote: ${info.lot} <br> Comprimento: ${info.comp} m <br> Largura: ${info.larg} m <br>
             Área: ${info.area} m² <br>
             Valor do lote: R$ ${info.valor.toFixed(2).replace('.', ',')}<br>
             Valor adicional: Isento <br>
-            Valor final: R$ ${info.valor.toFixed(2).replace('.', ',')} <br>`
+            Valor final: R$ ${info.valor.toFixed(2).replace('.', ',')} <br> <br>`
         }
 
         else if (info.valor <= 100000) {
             return `Descrição <br> Quadra: ${info.quadra} <br>
-            Lote: ${info.lot} <br> Comprimento: ${info.comp} <br> Largura: ${info.larg} <br>
+            Lote: ${info.lot} <br> Comprimento: ${info.comp} m <br> Largura: ${info.larg} m <br>
             Área: ${info.area} m² <br>
             Valor do lote: R$ ${info.valor.toFixed(2).replace('.', ',')}<br>
             Valor adicional: R$ ${(info.valor * 0.05).toFixed(2).replace('.', ',')} <br>
-            Valor final: R$ ${(info.valor + (info.valor * 0.05)).toFixed(2).replace('.', ',')} <br>`
+            Valor final: R$ ${(info.valor + (info.valor * 0.05)).toFixed(2).replace('.', ',')} <br> <br>`
         }
 
         else if (info.valor <= 500000) {
             return `Descrição <br> Quadra: ${info.quadra} <br>
-            Lote: ${info.lot} <br> Comprimento: ${info.comp} <br> Largura: ${info.larg} <br>
+            Lote: ${info.lot} <br> Comprimento: ${info.comp} m <br> Largura: ${info.larg} m <br>
             Área: ${info.area} m² <br>
             Valor do lote: R$ ${(info.valor).toFixed(2).replace('.', ',')}<br>
             Valor adicional: R$ ${(info.valor * 0.1).toFixed(2).replace('.', ',')} <br>
-            Valor final: R$ ${(info.valor + (info.valor * 0.1)).toFixed(2).replace('.', ',')} <br>`
+            Valor final: R$ ${(info.valor + (info.valor * 0.1)).toFixed(2).replace('.', ',')} <br> <br>`
         }
 
         else if (info.valor < 1000000) {
             return `Descrição <br> Quadra: ${info.quadra} <br>
-            Lote: ${info.lot} <br> Comprimento: ${info.comp} <br> Largura: ${info.larg} <br>
+            Lote: ${info.lot} <br> Comprimento: ${info.comp} m <br> Largura: ${info.larg} m <br>
             Área: ${info.area} m² <br>
             Valor do lote: R$ ${(info.valor).toFixed(2).replace('.', ',')}<br>
             Valor adicional: R$ ${(info.valor * 0.15).toFixed(2).replace('.', ',')} <br>
-            Valor final: R$ ${(info.valor + (info.valor * 0.15)).toFixed(2).replace('.', ',')} <br>`
+            Valor final: R$ ${(info.valor + (info.valor * 0.15)).toFixed(2).replace('.', ',')} <br> <br>`
         }
 
         else{
-            return `Descrição <br> Quadra: ${info.quadra} <br>
-            Lote: ${info.lot} <br> Comprimento: ${info.comp} <br> Largura: ${info.larg} <br>
+            return `Descrição <br> Quadra:  ${info.quadra} <br>
+            Lote: ${info.lot} <br> Comprimento: ${info.comp} m <br> Largura: ${info.larg} m <br>
             Área: ${info.area} m² <br>
-            Valor do lote: R$ ${info.valor}<br>
+            Valor do lote: R$ ${(info.valor).toFixed(2).replace('.', ',')}<br>
             Valor adicional: R$ ${(info.valor * 0.2).toFixed(2).replace('.', ',')} <br>
-            Valor final: R$ ${(info.valor + (info.valor * 0.2)).toFixed(2).replace('.', ',')} <br>`
+            Valor final: R$ ${(info.valor + (info.valor * 0.2)).toFixed(2).replace('.', ',')} <br> <br>`
         }
 
     }
